aebf66e4ae2dc24f897b46bfa7e0c3ceec96e409
Cherise222019313 <160508848+Cherise222019313@users.noreply.github.com>
2024-02-20 15:46:46 +0200

    Update db.java

--- Hostel Information Managment System JAVA PROJECT/src/student/information/system/db.java
+++ Hostel Information Managment System JAVA PROJECT/src/student/information/system/db.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package student.information.system;
 
-/**
- *
- * @author Hyrex
- */
+
 import java.sql.*;
 import javax.swing.*;
 public class db {
@@ -19,8 +12,7 @@ public class db {
         try{
             Class.forName("com.mysql.jdbc.Driver");
             Connection conn =DriverManager.getConnection("jdbc:mysql://localhost/studentinfo", "root", "");
-            //JOptionPane.showMessageDialog(null, "Connection to database is successful");
-      
+       
             return conn;
            
             
