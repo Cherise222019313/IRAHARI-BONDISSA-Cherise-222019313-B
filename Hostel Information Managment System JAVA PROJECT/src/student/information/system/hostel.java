b368f7e7155d2f7bd2f9da0bfeeeab0fd05dfe31
Cherise222019313 <160508848+Cherise222019313@users.noreply.github.com>
2024-02-20 15:47:20 +0200

    Update hostel.java

--- Hostel Information Managment System JAVA PROJECT/src/student/information/system/hostel.java
+++ Hostel Information Managment System JAVA PROJECT/src/student/information/system/hostel.java
@@ -1,14 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * /*
-Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package student.information.system;
 
-/**
- *
- * @author HP
- */
+package student.information.system;
+ 
 class hostel {
     
 }
